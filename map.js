63b79da2e84a3f2156ad63fd5c027c0f03edac8c
Frankie Michielli <fcm2118@columbia.edu>
2026-03-26 22:25:08 -0400

    first filled out map

--- map.js
+++ map.js
@@ -12,3 +12,62 @@ var map = new mapboxgl.Map({
     center: [-73.93324, 40.80877],
     zoom: 14
 });
+
+var blocks_url = "data/base_layer.geojson"
+var trees_url = "data/SBS_data.geojson"
+
+map.on('load',function(){
+    // define a 'source' for your polygons dataset
+    map.addSource('blocks_data',{
+      'type':'geojson',
+      'data': blocks_url,
+    });
+    // add a new layer with your polygons
+    map.addLayer({
+        'id':'blocks',
+        'type':'fill',
+        'source':'blocks_data',
+        'paint':{
+            'fill-color': 
+              ['case', 
+              ['==', ['get', 'avg_diam'], null],
+              'white',
+              ['step', ['get', 'avg_diam'],
+                '#ffffff',
+                2.615, '#edf8e9',
+                6.444, '#bae4b3',
+                9.379, '#74c476',
+                15.036, '#31a354',
+                26.000, '#006d2c'
+              ]],
+          'fill-outline-color':'#000000',
+          'fill-opacity': 0.5
+        }
+      })
+
+    // define a 'source' for your point dataset
+    map.addSource('trees_data',{
+    'type':'geojson',
+    'data': trees_url
+    });
+   // add a new layer with your points
+    map.addLayer({
+    'id':'trees',
+    'type':'circle',
+    'source':'trees_data',
+    'paint':{
+      'circle-color': ['match', 
+        ['get', 'Certification'],
+        'MBE,WBE', '#3b82f6',
+        'MBE', '#8b5cf6',
+        'WBE', '#facc15',
+        '#cccccc'],
+      'circle-opacity':0.8,
+      'circle-radius':4
+    },
+  })
+  });
+
+
+
+
